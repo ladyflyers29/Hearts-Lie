8e457f107a726c97cabbafc372bff1d359d84bc5
Riley Winkler <rex8112@gmail.com>
2019-01-26 01:29:17 -0600

    Initialized PlayerState.cs
    Co-Authored-By: ladyflyers29 <ladyflyers29@users.noreply.github.com>

--- Where The Heart Is/Assets/Scripts/PlayerState.cs
+++ Where The Heart Is/Assets/Scripts/PlayerState.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class PlayerState : MonoBehaviour
 {
+    [Header("Variables")]
+    [Tooltip("Level of Insanity")]
     public int insanity = 0;
+    [Tooltip("Hearts you are currently holding")]
     public int hearts = 0;
-    public int night = 0;
+    [Tooltip("All living potential targets")]
     public List<GameObject> targets = new List<GameObject>();
+    [Header("Debug")]
+    [Tooltip("Current Night")]
+    public int night = 0;
+    [Tooltip("Currently Active Targets")]
     public List<GameObject> activeTargets = new List<GameObject>();
 
     // Start is called before the first frame update
@@ -24,6 +31,9 @@ public class PlayerState : MonoBehaviour
 
     void selectTargets(int amt)
     {
-
+        for (int i = 0; i < amt; i++)
+        {
+            activeTargets.Add(targets[Random.Range(0, targets.Count - 1)]);
+        }
     }
 }
