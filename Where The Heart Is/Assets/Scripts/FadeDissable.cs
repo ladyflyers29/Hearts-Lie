ac3e72db54c5a891073e0f686c7ee9a7fdcc41ef
Jackson-Augspurger <36752136+Jackson-Augspurger@users.noreply.github.com>
2019-01-26 13:04:35 -0600

    menustuff
    more

---
+++ Where The Heart Is/Assets/Scripts/FadeDissable.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+
+public class FadeDissable : MonoBehaviour
+{
+
+
+   // void Start()
+   // {
+   //    StartCoroutine(Fade());
+   // }
+
+   // private IEnumerator Fade()
+ //  {
+  //      print(Time.time);
+  //          yield return WaitForSeconds(1);
+  //      print(Time.time);
+   // }
+
+}
+
+    
+
+    
+
+    // Update is called once per frame
+    //void Update()
+   // {
+        
+  //  }
+//}
