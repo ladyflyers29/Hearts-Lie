5ed5f5c73e3b57b32030d40ae2985de2b7847207
Jackson-Augspurger <36752136+Jackson-Augspurger@users.noreply.github.com>
2019-01-27 13:12:42 -0600

    UI
    UI is complete. Probably have to fix later for some reason

---
+++ Where The Heart Is/Assets/Scripts/FadeCompleteLV0.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FadeCompleteLV0 : MonoBehaviour
+{
+
+    public Animator animation;
+
+    private int loadLevel;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            FadeToLevel(3);
+        }
+    }
+
+    public void FadeToLevel (int levelIndex)
+    {
+        loadLevel = levelIndex;
+        animation.SetTrigger("FadeOut0");
+    }
+
+    public void OnFadeComplete()
+    {
+        SceneManager.LoadScene(loadLevel);
+    }
+}
---
+++ Where The Heart Is/Assets/Scripts/FadeDisclaim.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FadeDisclaim : MonoBehaviour
+{
+    public Animator animation;
+
+    private int loadLevel;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            FadeToLevel(4);
+        }
+    }
+
+    public void FadeToLevel(int levelIndex)
+    {
+        loadLevel = levelIndex;
+        animation.SetTrigger("FadeOutD");
+    }
+
+    public void OnFadeComplete()
+    {
+        SceneManager.LoadScene(loadLevel);
+    }
+}
+
