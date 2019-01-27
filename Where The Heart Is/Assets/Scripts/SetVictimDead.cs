f3c8bf356a0d9f4e76cef1860bcc25a9fd115c22
blue1442 <dominicsharma1@gmail.com>
2019-01-27 13:07:33 -0600

    AJ now Dies

--- Where The Heart Is/Assets/Scripts/SetVictimDead.cs
+++ Where The Heart Is/Assets/Scripts/SetVictimDead.cs
@@ -4,16 +4,49 @@ using UnityEngine;
 
 public class SetVictimDead : MonoBehaviour
 {
-    public 
+    private Animator animator;
+    private AudioSource death;
+    public bool dead = false;
+    //Rigidbody2D rb;
+
+
+    private VictimAI waypoint;
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+ 
+//        thisobject = GetComponent<GameObject>();
+        animator = GetComponent<Animator>();
+        waypoint = GetComponent<VictimAI>();
+        AudioSource[] sounds = GetComponents<AudioSource>();
 
+        Debug.Log("2");
+        death = sounds[1];
+        Debug.Log("3");
+
+       // rb = GetComponent<Rigidbody2D>();
+    }
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("hey i touched you");
+        if (collision.gameObject.tag == "knife")
+        {
+            Debug.Log("hey i got you");
+            animator.SetBool("isDead", true);
+            //rb.constraints = RigidbodyConstraints2D.FreezeAll;
+            waypoint.enabled = false;
+            death.Play();
+
+
+            //dead = true;
+
+
+        }
     }
 }
