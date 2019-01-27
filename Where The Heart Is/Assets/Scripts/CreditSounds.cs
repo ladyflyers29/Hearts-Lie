7beeee116e9bfd80c3626fea2553d07dd68ebf18
Brooke Gronli <42610874+ladyflyers29@users.noreply.github.com>
2019-01-27 13:40:48 -0600

    credit scene

---
+++ Where The Heart Is/Assets/Scripts/CreditSounds.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditSounds : MonoBehaviour
+{
+
+    public GameObject FastSound;
+    public GameObject NormalMusic;
+
+    void start()
+    {
+        FastSound = GameObject.Find("Fast Music");
+        NormalMusic = GameObject.Find("Actual Music");
+    }
+    public void StartFastSound()
+    {
+        NormalMusic.GetComponent<AudioSource>().mute = true;
+        FastSound.GetComponent<AudioSource>().mute = false;
+    }
+
+    public void ChangeSceneByName(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+}
