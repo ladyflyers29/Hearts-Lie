df34d75449d856d3a1c07dd61d9dbb440d5bb887
Jackson-Augspurger <36752136+Jackson-Augspurger@users.noreply.github.com>
2019-01-26 18:19:18 -0600

    menu
    Its done! next credits

---
+++ Where The Heart Is/Assets/Scripts/LevelToLoad.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelToLoad : MonoBehaviour
+{
+
+    public int levelToLoad;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    public void LoadLevel()
+    {
+        if (levelToLoad != 0)
+        {
+            SceneManager.LoadScene(levelToLoad);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
